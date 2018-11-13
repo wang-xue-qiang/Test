6ced770b1e8e6fcdcd836f49fafd71844bc6ca11
SayHelloAllen <592470261@qq.com>
2018-11-13 21:22:26 +0800

    first commit

---
+++ src/main/java/com/zkh/Apple.java
@@ -0,0 +1,19 @@
+package com.zkh;
+
+public class Apple {
+	private String name ="Hello wangxueqiang";
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+}
